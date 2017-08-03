de9be0609580838e854ec375bbc5106e940c444e
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-08-03 16:57:03 +1000

    Update UsageRecord.java
    Added files via upload

--- src/bcccp/tickets/season/UsageRecord.java
+++ src/bcccp/tickets/season/UsageRecord.java
@@ -9,31 +9,40 @@ public class UsageRecord implements IUsageRecord {
 	
 	
 	public UsageRecord(String ticketId, long startDateTime) {
+            this.ticketId = ticketId;
+            this.startDateTime = startDateTime;
 		//TODO Implement constructor
 	}
 
 
-
+/**
+ * @see bcccp.tickets.season.IUsageRecord#getEndTime() 
+ */
 	@Override
-	public void finalise(long endDateTime) {
+	public void finalise(long endTime) {
 		// TODO Auto-generated method stub
+                endDateTime = endTime;
 		
 	}
 
 
-
+/**
+ * @see bcccp.tickets.season.IUsageRecord#getStartTime() 
+ */
 	@Override
 	public long getStartTime() {
 		// TODO Auto-generated method stub
-		return 0;
+		return startDateTime;
 	}
 
 
-
+/**
+ * @see bcccp.tickets.season.IUsageRecord#getEndTime() 
+ */
 	@Override
 	public long getEndTime() {
 		// TODO Auto-generated method stub
-		return 0;
+		return endDateTime;
 	}
 
 
@@ -41,7 +50,7 @@ public class UsageRecord implements IUsageRecord {
 	@Override
 	public String getSeasonTicketId() {
 		// TODO Auto-generated method stub
-		return null;
+		return ticketId;
 	}
 	
 	
