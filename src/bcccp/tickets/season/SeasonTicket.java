264190c754ac77cfe7bd7072521aa1073fe2554a
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-08-03 16:56:03 +1000

    Update SeasonTicket.java
    Added files via upload

--- src/bcccp/tickets/season/SeasonTicket.java
+++ src/bcccp/tickets/season/SeasonTicket.java
@@ -3,7 +3,11 @@ package bcccp.tickets.season;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Date;
 
+/**
+ * Class SeasonTicket
+ */
 public class SeasonTicket implements ISeasonTicket {
 	
 	private List<IUsageRecord> usages;
@@ -14,59 +18,80 @@ public class SeasonTicket implements ISeasonTicket {
 	private long startValidPeriod;
 	private long endValidPeriod;
 	
+        /**
+         * Instantiates new Season Ticket
+         * @param ticketId
+         * @param carparkId
+         * @param startValidPeriod
+         * @param endValidPeriod 
+         */
 	public SeasonTicket (String ticketId, 
 			             String carparkId, 
 			             long startValidPeriod,
 			             long endValidPeriod) {
 		//TDO Implement constructor
+                this.ticketId = ticketId;
+		this.carparkId = carparkId;
+		this.startValidPeriod = startValidPeriod;
+		this.endValidPeriod = endValidPeriod;
 	}
-
+/**
+ * @see bcccp.tickets.season.ISeasonTicket#getId() 
+ */
 	@Override
 	public String getId() {
 		// TODO Auto-generated method stub
-		return null;
+		return ticketId;
 	}
-
+/**
+ * @see bcccp.tickets.season.ISeasonTicket#getCarparkId() 
+ */
 	@Override
 	public String getCarparkId() {
 		// TODO Auto-generated method stub
-		return null;
+		return carparkId;
 	}
-
+/**
+ * @see bcccp.tickets.season.ISeasonTicket#getStartValidPeriod() 
+ */
 	@Override
 	public long getStartValidPeriod() {
 		// TODO Auto-generated method stub
-		return 0;
+		return startValidPeriod;
 	}
-
+/**
+ * @see bcccp.tickets.season.ISeasonTicket#getEndValidPeriod() 
+ */
 	@Override
 	public long getEndValidPeriod() {
 		// TODO Auto-generated method stub
-		return 0;
+		return endValidPeriod;
 	}
 
 	@Override
 	public boolean inUse() {
 		// TODO Auto-generated method stub
-		return false;
+                return inUse();
+		
 	}
 
 	@Override
 	public void recordUsage(IUsageRecord record) {
 		// TODO Auto-generated method stub
+                record = currentUsage;
 		
 	}
 
 	@Override
 	public IUsageRecord getCurrentUsageRecord() {
 		// TODO Auto-generated method stub
-		return null;
+		return currentUsage;
 	}
 
 	@Override
 	public void endUsage(long dateTime) {
 		// TODO Auto-generated method stub
-		
+		 
 	}
 
 	@Override
