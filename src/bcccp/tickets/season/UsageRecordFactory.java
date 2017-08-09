9af8f8a284875bfa67447e95d0b4e2fad76dc7d5
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-08-09 15:38:16 +1000

    Implemented method functionality and added javadocs

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -2,11 +2,16 @@ package bcccp.tickets.season;
 
 public class UsageRecordFactory implements IUsageRecordFactory {
 
+    /**
+     * Constructor uses given ticketId and startDateTime to create new usage record
+     * @param ticketId
+     * @param startDateTime
+     * @return UsageRecord
+     */
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-		// TODO Auto-generated method stub
-		return null;
+               
+            return new UsageRecord(ticketId, startDateTime);              
 	}
-
 
 }
