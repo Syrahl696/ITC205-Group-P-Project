6220042a69d69ade1b5a03e154ff00e3833e2e88
Syrahl696 <Syrahl696@gmail.com>
2017-08-15 18:35:49 +1000

    Bugfix: Initialised the CurrentTicketList
    Initialised the CurrentTicketList in the AdhocTicketDAO constructor

--- src/bcccp/tickets/adhoc/AdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/AdhocTicketDAO.java
@@ -1,5 +1,6 @@
 package bcccp.tickets.adhoc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AdhocTicketDAO  implements IAdhocTicketDAO  {
@@ -12,7 +13,8 @@ public class AdhocTicketDAO  implements IAdhocTicketDAO  {
 	//AdhocTicketDAO constructor. assigning factory and setting currentTicketNo = 1
 	public AdhocTicketDAO(IAdhocTicketFactory factory) {
                 this.factory = factory;
-                currentTicketNo = 1;
+                this.currentTicketNo = 1;
+                this.currentAdhocTickets = new ArrayList();
 	}
 
 
