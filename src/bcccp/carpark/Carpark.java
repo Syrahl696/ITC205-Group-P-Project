88daa4a630c89077cc8c47a43ca131f9188dac0f
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-08-15 16:29:08 +1000

    Merge branch 'master' into Corey-Schmetzer

--- src/bcccp/carpark/Carpark.java
+++ src/bcccp/carpark/Carpark.java
@@ -1,11 +1,14 @@
 package bcccp.carpark;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bcccp.tickets.adhoc.IAdhocTicket;
 import bcccp.tickets.adhoc.IAdhocTicketDAO;
 import bcccp.tickets.season.ISeasonTicket;
 import bcccp.tickets.season.ISeasonTicketDAO;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Carpark implements ICarpark {
 	
@@ -15,84 +18,136 @@ public class Carpark implements ICarpark {
 	private int numberOfCarsParked;
 	private IAdhocTicketDAO adhocTicketDAO;
 	private ISeasonTicketDAO seasonTicketDAO;
+        final long FIFTEEN_MINUTES = 900000;
+        final float FIFTEEN_MINUTE_PRICE = 1;
 	
-	
-	
-	public Carpark(String name, int capacity, 
+    /**
+     * Constructs a Carpark object with the name, capacity, and the data access objects passed to it.
+     *
+     * @param name
+     * @param capacity
+     * @param adhocTicketDAO
+     * @param seasonTicketDAO
+     */
+    public Carpark(String name, int capacity, 
 			IAdhocTicketDAO adhocTicketDAO, 
 			ISeasonTicketDAO seasonTicketDAO) {
-		//TODO Implement constructor
+            this.carparkId = name;
+            this.capacity = capacity;
+            this.numberOfCarsParked = 0;
+            this.seasonTicketDAO = seasonTicketDAO;
+            this.adhocTicketDAO = adhocTicketDAO;
+            this.observers = new ArrayList<>();
 	}
 
-
-
-	@Override
+    /**
+     * Registers the passed object (usually an entry controller) as an observer to this carpark.
+     * @param observer
+     */
+    @Override
 	public void register(ICarparkObserver observer) {
-		// TODO Auto-generated method stub
+		if (!observers.contains(observer)) {
+			observers.add(observer);
+		}
 		
 	}
 
-
-
-	@Override
+    /**
+     * Removes the passed object (usually an entry controller) as an observer to this carpark.
+     * @param observer
+     */
+    @Override
 	public void deregister(ICarparkObserver observer) {
-		// TODO Auto-generated method stub
+		if (observers.contains(observer)) {
+			observers.remove(observer);
+		}
 		
 	}
 
-
-
-	@Override
+    /**
+     *Returns the name of this carpark.
+     * @return
+     */
+    @Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.carparkId;
 	}
 
-
-
-	@Override
+    /**
+     * Returns true if the carpark cannot accept any more ad-hoc ticket customers.
+     * @return
+     */
+    @Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		// TODO Include logic to reserve spots for Season Ticket holders
+		return (numberOfCarsParked >= capacity);
+        }
 
 
 
+        //create and return new adhoc ticket
 	@Override
 	public IAdhocTicket issueAdhocTicket() {
-		// TODO Auto-generated method stub
-		return null;
+
+            return adhocTicketDAO.createTicket(carparkId);
 	}
 
+    /**
+     * Also notifies all observers, allowing them to take an action if the carpark is full.
+     */
+    @Override
 
+	public void recordAdhocTicketEntry(IAdhocTicket ticket) {
+            
+            adhocTicketDAO.addToCurrentList(ticket);
+            numberOfCarsParked++;
+            if (this.isFull()){ //If the carpark is full, notify all observers. Entry pillars will then display carpark full.
 
-	@Override
-	public void recordAdhocTicketEntry() {
-		// TODO Auto-generated method stub
+                for (int i = 0; i < observers.size(); i++){
+                    observers.get(i).notifyCarparkEvent();
+                }
+
+            }
 		
 	}
 
 
 
 	@Override
-	public IAdhocTicket getAdhocTicket(String barcode) {
-		// TODO Auto-generated method stub
-		return null;
+
+	public IAdhocTicket getAdhocTicket(String barcode) { 
+            //return adhocTicket object, or null if not found
+		return adhocTicketDAO.findTicketByBarcode(barcode);
+
+	}
+
+
+
+        //decided on calculating per 15 minutes, with a charge of $4 an hour
+	@Override
+	public float calculateAdHocTicketCharge(long entryDateTime) {
+            long stayTime = System.currentTimeMillis() - entryDateTime;
+            System.out.println("Current time: " + System.currentTimeMillis() + "  entryDateTime: " + entryDateTime + "  stayTime: " + stayTime);
+            
+            float fifteenMinuteLotsStayed = (stayTime / FIFTEEN_MINUTES) + 1;
+            System.out.println("amount of fifteen minute lots: " + fifteenMinuteLotsStayed);
+            
+            return fifteenMinuteLotsStayed * FIFTEEN_MINUTE_PRICE;
 	}
 
 
 
 	@Override
-	public float calculateAddHocTicketCharge(long entryDateTime) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
+	public void recordAdhocTicketExit(IAdhocTicket ticket) {
+            numberOfCarsParked--;
+            
+            adhocTicketDAO.removeFromCurrentList(ticket);
 
+                for (int i = 0; i < observers.size(); i++){
+                        observers.get(i).notifyCarparkEvent();
+                }
 
-	@Override
-	public void recordAdhocTicketExit() {
-		// TODO Auto-generated method stub
 		
 	}
 
