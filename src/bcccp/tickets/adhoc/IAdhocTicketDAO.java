a9234c71c8b8dac9e40d89e5ee8e5790a4994506
aaronpeachey3 <aaron.peachey3@yahoo.com>
2017-08-29 18:14:25 +1000

    removed ListMethods
    removed the list methods that controlled the adding to and removing of the currentticketlist. No longer required.

--- src/bcccp/tickets/adhoc/IAdhocTicketDAO.java
+++ src/bcccp/tickets/adhoc/IAdhocTicketDAO.java
@@ -8,8 +8,6 @@ public interface IAdhocTicketDAO {
 	public IAdhocTicket findTicketByBarcode(String barcode);
 	public List<IAdhocTicket> getCurrentTickets();
 
-    public void addToCurrentList(IAdhocTicket ticket);
-    public void removeFromCurrentList(IAdhocTicket ticket);
 
 
 }
