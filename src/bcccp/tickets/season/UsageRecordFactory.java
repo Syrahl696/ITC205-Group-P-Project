33d6d62e46b876c8c5e8944f48d807b35884daa9
Corey-Schmetzer <corey.schmetzer@gmail.com>
2017-08-31 16:04:06 +1000

    Added error handling to UsageRecordFactory constructor
    Also updated respective javadocs

--- src/bcccp/tickets/season/UsageRecordFactory.java
+++ src/bcccp/tickets/season/UsageRecordFactory.java
@@ -4,13 +4,21 @@ public class UsageRecordFactory implements IUsageRecordFactory {
 
     /**
      * Constructor uses given ticketId and startDateTime to create new usage record
+     * @throws RuntimeException if ticketId is null or empty
+     * @throws RuntimeException if startDateTime is less than or equal to zero
      * @param ticketId
      * @param startDateTime
      * @return UsageRecord
      */
 	@Override
 	public IUsageRecord make(String ticketId, long startDateTime) {
-               
+               if (ticketId == null || ticketId == ""){
+                   throw new RuntimeException("ticket is null or empty");
+        }
+               //specification says "throws a RuntimeException is less than or equal to zero" , assumed startDateTime
+               if (startDateTime <= 0){
+                   throw new RuntimeException("startDateTime is less than or equal to zero");
+        }
             return new UsageRecord(ticketId, startDateTime);              
 	}
 
