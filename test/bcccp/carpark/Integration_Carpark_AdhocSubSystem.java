6acdfd7b4d91b179073d82bda75e5feb9e789e01
aaronpeachey3 <aaron.peachey3@yahoo.com>
2017-09-18 18:33:37 +1000

    2nd stage Integration Testing
    Added integration testing for AdhocTicket subSystem integrating with Carpark. 3 methods tested.

---
+++ test/bcccp/carpark/Integration_Carpark_AdhocSubSystem.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.carpark;
+import bcccp.tickets.adhoc.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import bcccp.tickets.season.*;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author PeacheyMacbook
+ */
+public class Integration_Carpark_AdhocSubSystem {
+    IAdhocTicket ticket;
+    IAdhocTicket ticket2;
+    IAdhocTicketDAO adhocDAO;
+    ISeasonTicketDAO seasonDAO;
+    ICarpark carpark;
+    
+    @Test
+    public void testCarparkIssueAdhocTicket() {
+        adhocDAO = new AdhocTicketDAO(new AdhocTicketFactory());
+        seasonDAO = new SeasonTicketDAO(new UsageRecordFactory());
+        carpark = new Carpark("test carpark", 5, 1, adhocDAO, seasonDAO);
+        
+        ticket = carpark.issueAdhocTicket();
+        ticket2 = new AdhocTicket("test carpark", 1, "barcode");
+        
+        assertEquals(ticket.getTicketNo(), ticket2.getTicketNo());
+        assertEquals(ticket.getCarparkId(), ticket2.getCarparkId());
+        
+        assertTrue(ticket.getEntryDateTime() > 0);
+        assertEquals((ticket.getBarcode()).substring(0, 2), "A1");   //same tests as the DAO, should it be something else?
+        
+    }
+    
+    @Test
+    public void testCarparkGetAdhocTicket() {
+        adhocDAO = new AdhocTicketDAO(new AdhocTicketFactory());
+        seasonDAO = new SeasonTicketDAO(new UsageRecordFactory());
+        carpark = new Carpark("test carpark", 5, 1, adhocDAO, seasonDAO);
+        
+        ticket = carpark.issueAdhocTicket();
+        ticket2 = carpark.getAdhocTicket(ticket.getBarcode());
+        
+        //test if tickets equal
+        assertEquals(ticket, ticket2);
+        
+    }
+    
+    //thorough testing on the calcCharge method has already been complted during unit testing
+    //this covered Business Hours, Out of Hours, Weekend and Multi Day stays
+    //this test will just ensure the system communicates from the CalculateAdhocTicketCharge to the calcCharge()
+    @Test
+    public void testCalculateAdhocTicketCharge() throws InterruptedException {
+        adhocDAO = new AdhocTicketDAO(new AdhocTicketFactory());
+        seasonDAO = new SeasonTicketDAO(new UsageRecordFactory());
+        carpark = new Carpark("test carpark", 5, 1, adhocDAO, seasonDAO);
+        
+        ticket = carpark.issueAdhocTicket();
+        
+        Date current = new Date();
+        float charge = carpark.calculateAdhocTicketCharge(current.getTime()-1000000);
+        
+        assertTrue(charge > 0);
+        
+    }
+    
+    
+    
+}
--- test/bcccp/tickets/adhoc/Integration_AdhocTicketDAO_FactoryTicket.java
+++ test/bcccp/tickets/adhoc/Integration_AdhocTicketDAO_FactoryTicket.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package bcccp.tickets.adhoc;
 import bcccp.tickets.adhoc.*;
 import org.junit.After;
 import org.junit.AfterClass;
