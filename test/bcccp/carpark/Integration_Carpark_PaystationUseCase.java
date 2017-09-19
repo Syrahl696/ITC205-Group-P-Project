81bb36b1d4aa9d5c8eb350af189952a75833634e
aaronpeachey3 <aaron.peachey3@yahoo.com>
2017-09-19 18:21:07 +1000

    begun final stage of integration testing

---
+++ test/bcccp/carpark/Integration_Carpark_PaystationUseCase.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bcccp.carpark;
+import bcccp.tickets.adhoc.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import bcccp.tickets.season.*;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author PeacheyMacbook
+ */
+public class Integration_Carpark_PaystationUseCase {
+    
+    //look at unit tests
+    
+}
